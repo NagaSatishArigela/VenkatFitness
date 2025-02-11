7935fdf68c1c57332a8f804e9894069d5db54573
NagaSatishArigela <nagasatisharigela@gmail.com>
2025-02-11 21:45:14 +0530

    cors issue fix

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: false, }));
 
 app.use('/uploads', express.static('uploads'));
 
